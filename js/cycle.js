f2b5541ccc4919ab011a02fcf115911092e9b203
Chris Muccioli <chris@kickstarter.com>
2016-08-30 00:11:25 -0400

    changes

--- js/cycle.js
+++ js/cycle.js
@@ -1,17 +1,1 @@
-jQuery(function($){
 
-  // Cycle plugin
-  $('.slides').cycle({
-      fx:     'none',
-      speed:   150,
-      timeout: 5
-  }).cycle("pause");
-
-  // Pause &amp; play on hover
-  $('.slideshow-block').hover(function(){
-    $(this).find('.slides').addClass('active').cycle('resume');
-  }, function(){
-    $(this).find('.slides').removeClass('active').cycle('pause');
-  });
-
-});
